443511d988145fa61ba893edd1d53fd9874ba343
MrChildrenJ <jj.chunchieh.huang@gmail.com>
2025-07-03 19:44:08 -0600

    clean files

---
+++ CMakeFiles/asteroids.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/asteroids.dir/src/asteroid.cpp.o"
+  "CMakeFiles/asteroids.dir/src/asteroid.cpp.o.d"
+  "CMakeFiles/asteroids.dir/src/game.cpp.o"
+  "CMakeFiles/asteroids.dir/src/game.cpp.o.d"
+  "CMakeFiles/asteroids.dir/src/laser.cpp.o"
+  "CMakeFiles/asteroids.dir/src/laser.cpp.o.d"
+  "CMakeFiles/asteroids.dir/src/main.cpp.o"
+  "CMakeFiles/asteroids.dir/src/main.cpp.o.d"
+  "CMakeFiles/asteroids.dir/src/ship.cpp.o"
+  "CMakeFiles/asteroids.dir/src/ship.cpp.o.d"
+  "CMakeFiles/asteroids.dir/src/window.cpp.o"
+  "CMakeFiles/asteroids.dir/src/window.cpp.o.d"
+  "bin/asteroids"
+  "bin/asteroids.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/asteroids.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
